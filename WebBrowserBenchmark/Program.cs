f436b76aed2788ddbfa413d79cb46c5b00aaccfc
tak o <asd>
2022-04-04 17:19:40 +0200

    Base project
    get list of processes, get list of edge processes and print them all.

---
+++ WebBrowserBenchmark/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+using System.ComponentModel;
+
+Process[] testProcess = Process.GetProcesses();
+Console.WriteLine("Lista pobranych procesów:");
+for(int i = 0; i < testProcess.Length; i++)
+{
+    Console.WriteLine(testProcess[i]);
+}
+
+Process[] listEdgeProcesses = Process.GetProcessesByName("msedge");
+Console.WriteLine();
+Console.WriteLine("Procesy edge");
+foreach(Process process in listEdgeProcesses)
+{
+    Console.WriteLine(process.NonpagedSystemMemorySize64);
+}
