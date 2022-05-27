08a8d616f7b740f6b056717fbeb70fc2c1f77e04
Adam <szlazakadam@gmail.com>
2022-05-27 10:51:34 +0200

    Psuje sie

---
+++ WebBrowserBenchmark/EdgeHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Selenium = OpenQA.Selenium;
+
+namespace WebBrowserBenchmark
+{
+    internal class EdgeHelper
+    {
+        public static void acceptCookie(Selenium.Edge.EdgeDriver driver)
+        {
+            string url = driver.Url;
+            
+            switch (url)
+            {
+                case string a when a.Contains("youtube"):
+                    driver.FindElement(Selenium.By.XPath("//*[@id='content']/div/div[6]/div[1]/ytd-button-renderer[2]/a")).Click();
+                    break;
+                default:
+                    break;
+            }
+        }
+        public static Selenium.Edge.EdgeDriver setEdge(string webEngineDirectory)
+        {
+            Selenium.Edge.EdgeOptions edgeEngine = new Selenium.Edge.EdgeOptions();
+
+            Selenium.Edge.EdgeDriver edgeDriver = new Selenium.Edge.EdgeDriver(webEngineDirectory);
+            return edgeDriver;
+        }
+        public static void openPage(Selenium.Edge.EdgeDriver driver, string pageName)
+        {
+            switch (pageName)
+            {
+                case "youtube":
+                    driver.Navigate().GoToUrl("Https://youtube.com");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
--- WebBrowserBenchmark/Program.cs
+++ WebBrowserBenchmark/Program.cs
@@ -1,60 +1,81 @@
-﻿using System.Diagnostics;
-using System.ComponentModel;
+﻿using System;
+using Selenium = OpenQA.Selenium;
 
-// variables to track memory usage
-long peakPagedMem = 0, peakWorkingSet = 0, peakVirtualMem = 0;
-
-Process[] myProcess = Process.GetProcessesByName("msedge");
-// Display the process statistics until
-// the user closes the program.
-for (int i = 0; i < myProcess.Length; i++)
+namespace WebBrowserBenchmark
 {
-    do
+    internal class Program
     {
-        if (!myProcess[i].HasExited)
+        static void Main(string[] args)
         {
-            myProcess[i].Refresh();
+            //// variables to track memory usage
+            //long peakPagedMem = 0, peakWorkingSet = 0, peakVirtualMem = 0;
 
-            Console.WriteLine($"{myProcess[i]}");
-            Console.WriteLine("--------------");
+            //Process[] myProcess = Process.GetProcessesByName("msedge");
+            //// Display the process statistics until
+            //// the user closes the program.
+            //for (int i = 0; i < myProcess.Length; i++)
+            //{
+            //    do
+            //    {
+            //        if (!myProcess[i].HasExited)
+            //        {
+            //            myProcess[i].Refresh();
 
-            Console.WriteLine($"  Physical memory usage     : {myProcess[i].WorkingSet64}");
-            Console.WriteLine($"  Base priority             : {myProcess[i].BasePriority}");
-            Console.WriteLine($"  Priority class            : {myProcess[i].PriorityClass}");
-            Console.WriteLine($"  User processor time       : {myProcess[i].UserProcessorTime}");
-            Console.WriteLine($"  Privileged processor time : {myProcess[i].PrivilegedProcessorTime}");
-            Console.WriteLine($"  Total processor time      : {myProcess[i].TotalProcessorTime}");
-            Console.WriteLine($"  Paged system memory size  : {myProcess[i].PagedSystemMemorySize64}");
-            Console.WriteLine($"  Paged memory size         : {myProcess[i].PagedMemorySize64}");
+            //            Console.WriteLine($"{myProcess[i]}");
+            //            Console.WriteLine("--------------");
 
-            if (peakPagedMem <= myProcess[i].PeakPagedMemorySize64)
-            {
-                peakPagedMem = myProcess[i].PeakPagedMemorySize64;
-            }
+            //            Console.WriteLine($"  Physical memory usage     : {myProcess[i].WorkingSet64}");
+            //            Console.WriteLine($"  Base priority             : {myProcess[i].BasePriority}");
+            //            Console.WriteLine($"  Priority class            : {myProcess[i].PriorityClass}");
+            //            Console.WriteLine($"  User processor time       : {myProcess[i].UserProcessorTime}");
+            //            Console.WriteLine($"  Privileged processor time : {myProcess[i].PrivilegedProcessorTime}");
+            //            Console.WriteLine($"  Total processor time      : {myProcess[i].TotalProcessorTime}");
+            //            Console.WriteLine($"  Paged system memory size  : {myProcess[i].PagedSystemMemorySize64}");
+            //            Console.WriteLine($"  Paged memory size         : {myProcess[i].PagedMemorySize64}");
 
-            if (peakVirtualMem <= myProcess[i].PeakVirtualMemorySize64)
-            {
-                peakVirtualMem = myProcess[i].PeakVirtualMemorySize64;
-            }
-            if (peakWorkingSet <= myProcess[i].PeakWorkingSet64)
-            {
-                peakWorkingSet = myProcess[i].PeakWorkingSet64;
-            }
-            if (myProcess[i].Responding)
-            {
-                Console.WriteLine("Status = Running");
-            }
-            else
-            {
-                Console.WriteLine("Status = Not Responding");
-            }
+            //            if (peakPagedMem <= myProcess[i].PeakPagedMemorySize64)
+            //            {
+            //                peakPagedMem = myProcess[i].PeakPagedMemorySize64;
+            //            }
+
+            //            if (peakVirtualMem <= myProcess[i].PeakVirtualMemorySize64)
+            //            {
+            //                peakVirtualMem = myProcess[i].PeakVirtualMemorySize64;
+            //            }
+            //            if (peakWorkingSet <= myProcess[i].PeakWorkingSet64)
+            //            {
+            //                peakWorkingSet = myProcess[i].PeakWorkingSet64;
+            //            }
+            //            if (myProcess[i].Responding)
+            //            {
+            //                Console.WriteLine("Status = Running");
+            //            }
+            //            else
+            //            {
+            //                Console.WriteLine("Status = Not Responding");
+            //            }
+            //        }
+            //    } while (!myProcess[i].WaitForExit(1000));
+            //}
+            //Console.WriteLine();
+
+
+            //// Display peak memory statistics for the process.
+            //Console.WriteLine("ddddupa");
+            //Console.WriteLine($"  Peak physical memory usage : {peakWorkingSet / (8 * 1024)}KB");
+            //Console.WriteLine($"  Peak paged memory usage    : {peakPagedMem / (8 * 1024)}KB");
+            //Console.WriteLine($"  Peak virtual memory usage  : {peakVirtualMem / (8 * 1024)}KB");
+
+            string webEngineDirectory = System.IO.Directory.GetCurrentDirectory();
+            webEngineDirectory = webEngineDirectory.Remove(webEngineDirectory.Length - 36, 36) + "WebEngines";
+
+            var edge = EdgeHelper.setEdge(webEngineDirectory);
+
+            //EdgeHelper.openPage(edge, "youtube");
+            //EdgeHelper.acceptCookie(edge);
+
+            //void openWebsite
         }
-    } while (!myProcess[i].WaitForExit(1000));
+
+    }
 }
-Console.WriteLine();
-
-
-// Display peak memory statistics for the process.
-Console.WriteLine($"  Peak physical memory usage : {peakWorkingSet / (8 * 1024)}KB");
-Console.WriteLine($"  Peak paged memory usage    : {peakPagedMem / (8 * 1024)}KB");
-Console.WriteLine($"  Peak virtual memory usage  : {peakVirtualMem / (8 * 1024)}KB");
